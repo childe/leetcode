7914752f6e502aaa8175d2f27065f94934708fa9
childe <heychilde@gmail.com>
2015-07-27 18:20:13 +0800

    O(n)
    参考https://github.com/haoel/leetcode/blob/master/algorithms/twoSum/twoSum.cpp

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -10,19 +10,11 @@ class Solution:
     # @return {integer[]}
 
     def twoSum(self, nums, target):
-        soted_nums = sorted(nums)
-        for idx_i, i in enumerate(soted_nums):
-            for idx_j, j in enumerate(soted_nums[idx_i+1:]):
-                if i+j == target:
-                    rst1 = nums.index(i)+1
-                    if j == i:
-                        rst2 = nums[rst1:].index(j)+rst1+1
-                    else:
-                        rst2 = nums.index(j)+1
-                    rst = sorted([rst1, rst2])
-                    return rst
-                elif i+j > target:
-                    break
+        d = {}
+        for idx, e in enumerate(nums):
+            if e in d:
+                return d[e], idx+1
+            d[target-e] = idx+1
 
 
 class TestSolution(unittest.TestCase):
