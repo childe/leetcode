f9b75ae75290bc14f432ab5a790ffc589ce26f9f
childe <heychilde@gmail.com>
2015-07-28 12:46:44 +0800

    use cut&tr; Memory Limit Exceeded

--- transpose-file/solution.sh
+++ transpose-file/solution.sh
@@ -15,5 +15,7 @@ do
     #awk -v i=$((1+index))  -v wc=$((0+wc)) '{if (NR==wc) space="\n"; else space=" "} {printf "%s%s",$i,space}' file.txt
     #awk -v i=$((1+index)) '{ if(1==NR){printf "%s",$i} else{printf " %s",$i} }END{printf "\n"}' file.txt
     #awk -v i=$((1+index))  '{if (NR==1) space=""; else space=" "} {printf "%s%s",space,$i} END {printf "\n"}' file.txt
-    awk '{if (NR==1) space=""; else space=" "} {printf "%s%s",space,$'$((1+index))'} END {printf "\n"}' file.txt
+    #awk '{if (NR==1) space=""; else space=" "} {printf "%s%s",space,$'$((1+index))'} END {printf "\n"}' file.txt
+    cut -f $((1+index)) -d" " file.txt |tr "\n" " "
+    echo
 done
