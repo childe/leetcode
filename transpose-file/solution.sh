9b730be9477b663868a0fab3293f6212bcb032c2
childe <heychilde@gmail.com>
2015-07-30 19:09:15 +0800

    fix memory limit

--- transpose-file/solution.sh
+++ transpose-file/solution.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
-#columncount=$(head -n1 file.txt|awk '{print NF}')
-#echo $columncount
-#for i in {1..$((columncount+0))}
-#do
-    #echo $i
-#done
+awk '
+    {
+    for (i=1; i<=NF; i++){
+        s[NR,i] = $i;
+    }
+    f=NF
+    }
 
-
-read -a array <<< $(head -n1 file.txt)
-#wc=$(wc -l file.txt)
-for index in "${!array[@]}"
-do
-    #echo "$index ${array[index]}"
-    #awk -v i=$((1+index))  -v wc=$((0+wc)) '{if (NR==wc) space="\n"; else space=" "} {printf "%s%s",$i,space}' file.txt
-    #awk -v i=$((1+index)) '{ if(1==NR){printf "%s",$i} else{printf " %s",$i} }END{printf "\n"}' file.txt
-    #awk -v i=$((1+index))  '{if (NR==1) space=""; else space=" "} {printf "%s%s",space,$i} END {printf "\n"}' file.txt
-    #awk '{if (NR==1) space=""; else space=" "} {printf "%s%s",space,$'$((1+index))'} END {printf "\n"}' file.txt
-    cut -f $((1+index)) -d" " file.txt |tr "\n" " "
-    echo
-done
+    END{
+    for (i=1;i<=f;i++){
+        for(j=1;j<NR;j++){
+            printf("%s ",s[j,i])
+        }
+        printf("%s\n",s[j,i])
+    }
+    }
+' file.txt
