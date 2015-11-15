7c572a014501201da427fe086d3049a35cd38d4f
childe <heychilde@gmail.com>
2015-11-15 22:01:30 +0800

    format

--- find-minimum-in-rotated-sorted-array/solution.py
+++ find-minimum-in-rotated-sorted-array/solution.py
@@ -34,47 +34,48 @@ class Solution(object):
                 start = mid
         return min(nums[start:end+1])
 
+
 class testSolution(unittest.TestCase):
+
     def test_findMin(self):
         s = Solution()
 
         nums = []
-        self.assertEqual(None,s.findMin(nums))
+        self.assertEqual(None, s.findMin(nums))
 
         nums = [1]
-        self.assertEqual(1,s.findMin(nums))
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [1,2]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [1, 2]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [2,1]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [2, 1]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [1,2,3]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [1, 2, 3]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [2,3,1]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [2, 3, 1]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [3,1,2]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [3, 1, 2]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [2,3,4,1]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [2, 3, 4, 1]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [3,4,1,2]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [3, 4, 1, 2]
+        self.assertEqual(1, s.findMin(nums))
 
-        nums = [4,1,2,3]
-        self.assertEqual(1,s.findMin(nums))
+        nums = [4, 1, 2, 3]
+        self.assertEqual(1, s.findMin(nums))
 
         nums = range(100)
         for i in range(len(nums)):
-            self.assertEqual(0,s.findMin(nums[i:] + nums[:i]))
+            self.assertEqual(0, s.findMin(nums[i:] + nums[:i]))
         nums = range(101)
         for i in range(len(nums)):
-            self.assertEqual(0,s.findMin(nums[i:] + nums[:i]))
-
+            self.assertEqual(0, s.findMin(nums[i:] + nums[:i]))
 
 
 if __name__ == '__main__':
