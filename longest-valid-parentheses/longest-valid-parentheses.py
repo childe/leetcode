854c802fd992aecd3108f5f88c256416e0bc84b5
childe <heychilde@gmail.com>
2017-09-04 00:44:57 +0800

    Status: Accepted
    229 / 229 test cases passed.
    Runtime: 112 ms

--- longest-valid-parentheses/longest-valid-parentheses.py
+++ longest-valid-parentheses/longest-valid-parentheses.py
@@ -14,61 +14,45 @@ Another example is ")()())", where the longest valid parentheses substring is "(
 
 
 class Solution(object):
-    def _isValid(self, intermediate, s, start, startChar, end, endChar):
-        # print start, startChar, end, endChar
-        # if startChar == ')' or endChar == '(':
-            # return False
-
-        # ()
-        if end-start == 1:
-            return True
-
-        # (XX)
-        if intermediate[start+1] and intermediate[start+1][-1] == end-1:
-            return True
-
-        # (X)(X)
-        for i in intermediate[start]:
-            if intermediate[i+1] and intermediate[i+1][-1] == end:
-                return True
-
-        return False
 
     def longestValidParentheses(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if s == "":
-            return 0
-
-        intermediate = {}
-
-        # init
-        for i in range(len(s)):
-            intermediate[i] = []
-
-        # N
-        for end, endChar in enumerate(s):
-            if endChar == '(':
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(c)
                 continue
-            for start in range(end-1, -1, -1):
-                if s[start] == ')':
-                    continue
-                if self._isValid(
-                        intermediate,
-                        s,
-                        start,
-                        s[start],
-                        end,
-                        endChar):
-                    intermediate[start].append(end)
 
-        # find the result
+            current = 0
+            while stack:
+                poped = stack.pop()
+                if poped == '(':
+                    if c == ')':
+                        current += 2
+                        c = None
+                        continue
+                    else:  # c is None
+                        stack.append('(')
+                        break
+                elif poped == ')':
+                    stack.append(poped)
+                    break
+                else:  # number
+                    current += poped
+                    continue
+
+            if current != 0:
+                stack.append(current)
+            if c == ')':
+                stack.append(')')
+
         max_length = 0
-        for start, ends in intermediate.items():
-            if ends:
-                max_length = max(1+ends[-1]-start, max_length)
+        for e in stack:
+            if isinstance(e, int):
+                max_length = max(max_length, e)
 
         return max_length
 
@@ -77,25 +61,25 @@ class TestSolution(unittest.TestCase):
     def test_longestValidParentheses(self):
         solution = Solution()
 
-        # s = ""
-        # rst = solution.longestValidParentheses(s)
-        # self.assertEqual(0, rst)
+        s = ""
+        rst = solution.longestValidParentheses(s)
+        self.assertEqual(0, rst)
 
-        # s = "()"
-        # rst = solution.longestValidParentheses(s)
-        # self.assertEqual(2, rst)
+        s = "()"
+        rst = solution.longestValidParentheses(s)
+        self.assertEqual(2, rst)
 
-        # s = ")("
-        # rst = solution.longestValidParentheses(s)
-        # self.assertEqual(0, rst)
+        s = ")("
+        rst = solution.longestValidParentheses(s)
+        self.assertEqual(0, rst)
 
-        # s = ")()"
-        # rst = solution.longestValidParentheses(s)
-        # self.assertEqual(2, rst)
+        s = ")()"
+        rst = solution.longestValidParentheses(s)
+        self.assertEqual(2, rst)
 
-        # s = "()()"
-        # rst = solution.longestValidParentheses(s)
-        # self.assertEqual(4, rst)
+        s = "()()"
+        rst = solution.longestValidParentheses(s)
+        self.assertEqual(4, rst)
 
         s = "()()))())())))(()((()()()))())"
         rst = solution.longestValidParentheses(s)
