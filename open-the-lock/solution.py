140e9e6faeab02b45e420f02d3b3776a585fb91a
childe <heychilde@gmail.com>
2018-06-26 17:28:20 +0800

    tmp commit

---
+++ open-the-lock/solution.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+https://leetcode.com/problems/open-the-lock/description/
+
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+
+Example 1:
+Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+Output: 6
+Explanation:
+A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
+Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
+because the wheels of the lock become stuck after the display becomes the dead end "0102".
+Example 2:
+Input: deadends = ["8888"], target = "0009"
+Output: 1
+Explanation:
+We can turn the last wheel in reverse to move from "0000" -> "0009".
+Example 3:
+Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
+Output: -1
+Explanation:
+We can't reach the target without getting stuck.
+Example 4:
+Input: deadends = ["0000"], target = "8888"
+Output: -1
+Note:
+The length of deadends will be in the range [1, 500].
+target will not be in the list deadends.
+Every string in deadends and the string target will be a string of 4 digits from the 10,000 possibilities '0000' to '9999'.
+'''
+
+
+class Solution(object):
+    def nextNodes(self, node, deadends):
+        """
+        :type node: str
+        :type deadends: List[str]
+        :rtype: [str]
+
+        >>> s = Solution()
+        >>> sorted(list(s.nextNodes('0000', ['0001', '0090'])))
+        ['0009', '0010', '0100', '0900', '1000', '9000']
+        """
+        rst = set()
+        for i, c in enumerate(node):
+            n = int(c)
+            up = (n+1) % 10
+            up = node[:i]+str(up)+node[i+1:]
+            down = (n-1) % 10
+            down = node[:i]+str(down)+node[i+1:]
+
+            if up not in deadends:
+                rst.add(up)
+            if down not in deadends:
+                rst.add(down)
+
+        return rst
+
+    def openLock(self, deadends, target):
+        """
+        :type deadends: List[str]
+        :type target: str
+        :rtype: int
+        >>> s = Solution()
+        >>> s.openLock(["0201","0101","0102","1212","2002"], target = "0202")
+        6
+        >>> s.openLock(deadends = ["8888"], target = "0009")
+        1
+        >>> s.openLock(deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888")
+        -1
+        >>> s.openLock(deadends = ["0000"], target = "8888")
+        -1
+        """
+
+        if '0000' in deadends:
+            return -1
+
+        if '0000' == target:
+            return 0
+
+        # init
+        nodes_s = set(['0000'])
+        nodes_d = set([target])
+        visited_from_source = {'0000': 0}
+        visited_from_target = {target: 0}
+
+        # bfs
+        while True:
+            if not nodes_s:
+                return -1
+            nodes_in_next_step = set()
+            while nodes_s:
+                node = nodes_s.pop()
+                steps = visited_from_source[node]
+
+                for node in self.nextNodes(node, deadends):
+                    if node in visited_from_target:
+                        return steps + visited_from_target[node] + 1
+                    if node in visited_from_source:
+                        continue
+                    visited_from_source[node] = steps+1
+                    nodes_in_next_step.add(node)
+            nodes_s = nodes_in_next_step
+
+            if not nodes_d:
+                return -1
+            nodes_in_next_step = set()
+            while nodes_d:
+                node = nodes_d.pop()
+                steps = visited_from_target[node]
+
+                for node in self.nextNodes(node, deadends):
+                    if node in visited_from_source:
+                        return steps + visited_from_source[node] + 1
+                    if node in visited_from_target:
+                        continue
+                    visited_from_target[node] = steps+1
+                    nodes_in_next_step.add(node)
+            nodes_d = nodes_in_next_step
+
+        return -1
+
+
+if __name__ == '__main__':
+    import time
+    print time.time()
+    s = Solution()
+    for l in open('testcase.txt'):
+        if not l.strip():
+            continue
+        l = [e.strip() for e in l.strip().split(",")]
+        deadends, target = l[:-1], l[-1]
+        print s.openLock(deadends, target)
+    print time.time()
---
+++ open-the-lock/solution2.py
@@ -0,0 +1,51 @@
+from collections import deque
+class Solution(object):
+    def openLock(self, deadends, target):
+        """
+        :type deadends: List[str]
+        :type target: str
+        :rtype: int
+        """
+        initial = "0000"
+        if initial in deadends:
+            return -1
+        status = deque([(initial, 0)])
+        visited = set(deadends)
+        visited.add(initial)
+
+        while status:
+            current_status, step = status.popleft()
+            # print current_status, step
+            for i in range(0, 4):
+                for j in [-1, 1]:
+                    # print j
+                    next_status = current_status[0:i] + str((int(current_status[i]) - int("0") + j)%10) + current_status[i+1:]
+                    if next_status in visited:
+                        continue
+                    else:
+                        if next_status == target:
+                            return step + 1
+                        status.append([next_status, step+1])
+                        visited.add(next_status)
+
+        return -1
+
+if __name__ == '__main__':
+    # s = Solution()
+    # print s.openLock(["0201","0101","0102","1212","2002"], target = "0202")
+
+    # print s.openLock(deadends = ["8888"], target = "0009")
+
+    # print s.openLock(deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888")
+
+    # print s.openLock(deadends = ["0000"], target = "8888")
+    import time
+    print time.time()
+    s = Solution()
+    for l in open('testcase.txt'):
+        if not l.strip():
+            continue
+        l = [e.strip() for e in l.strip().split(",")]
+        deadends, target = l[:-1], l[-1]
+        print s.openLock(deadends, target)
+    print time.time()
