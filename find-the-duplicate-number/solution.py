978e20632efe5ef4bf6795643ef26f7a5b86f632
childe <heychilde@gmail.com>
2018-08-16 18:47:19 +0800

    Status: Accepted
    53 / 53 test cases passed.
    Runtime: 24 ms
    new file:   find-the-duplicate-number/solution.py

---
+++ find-the-duplicate-number/solution.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+https://leetcode.com/problems/find-the-duplicate-number/description/
+
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+"""
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        >>> s = Solution()
+        >>> s.findDuplicate([1,3,4,2,2])
+        2
+        >>> s.findDuplicate([3,1,3,4,2])
+        3
+        """
+        l = len(nums)
+        for i, n in enumerate(nums):
+            n = n % l
+            if nums[n-1] > l:
+                return n
+            nums[n-1] += l
