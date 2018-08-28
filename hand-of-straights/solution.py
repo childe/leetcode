d52e91e38f437bf2e77d23ec97f05533dc61fca5
childe <heychilde@gmail.com>
2018-08-28 16:29:22 +0800

    new file:   hand-of-straights/solution.py

---
+++ hand-of-straights/solution.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+https://leetcode.com/problems/hand-of-straights/description/
+
+Alice has a hand of cards, given as an array of integers.
+
+Now she wants to rearrange the cards into groups so that each group is size W, and consists of W consecutive cards.
+
+Return true if and only if she can.
+
+
+
+Example 1:
+
+Input: hand = [1,2,3,6,2,3,4,7,8], W = 3
+Output: true
+Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8].
+Example 2:
+
+Input: hand = [1,2,3,4,5], W = 4
+Output: false
+Explanation: Alice's hand can't be rearranged into groups of 4.
+
+
+Note:
+
+1 <= hand.length <= 10000
+0 <= hand[i] <= 10^9
+1 <= W <= hand.length
+"""
+
+
+class Solution(object):
+    def isNStraightHand2(self, hand, W):
+        """
+        :type hand: List[int]
+        :type W: int
+        :rtype: bool
+        """
+        if hand == []:
+            return True
+        m = hand[0]
+        for n in range(m, m+W):
+            if n in hand:
+                hand.remove(n)
+            else:
+                return False
+        return self.isNStraightHand2(hand, W)
+
+    def isNStraightHand(self, hand, W):
+        """
+        :type hand: List[int]
+        :type W: int
+        :rtype: bool
+        >>> s = Solution()
+        >>> s.isNStraightHand(hand = [1,2,3,6,2,3,4,7,8], W = 3)
+        True
+        >>> s.isNStraightHand(hand = [1,2,3,4,5], W = 4)
+        False
+        """
+        if len(hand) % W != 0:
+            return False
+        hand.sort()
+        return self.isNStraightHand2(hand, W)
