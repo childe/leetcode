d5eacfaa2451cee50ae053270203c8f4b256e117
childe <heychilde@gmail.com>
2018-09-20 14:44:18 +0800

    Status: Accepted
    20 / 20 test cases passed.
    Runtime: 192 ms
    
    modified:   all-possible-full-binary-trees/solution.py

--- all-possible-full-binary-trees/solution.py
+++ all-possible-full-binary-trees/solution.py
@@ -13,6 +13,8 @@ You may return the final list of trees in any order.
 
 Input: 7
 Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+
+1 <= N <= 20
 """
 
 # Definition for a binary tree node.
@@ -26,51 +28,6 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def serialize(self, root):
-        """
-        :type root: TreeNode
-        :rtype: list[int]
-        >>> s = Solution()
-        >>> n = TreeNode(0)
-        >>> s.serialize(n)
-        '0,,'
-        >>> n.left = TreeNode(0)
-        >>> n.left.left = TreeNode(0)
-        >>> n.left.right = TreeNode(0)
-        >>> s.serialize(n)
-        '0,0,,0,0,,,,'
-        >>> n.right = TreeNode(0)
-        >>> s.serialize(n)
-        '0,0,0,0,0,,,,,,'
-        """
-        rst = []
-        s = [root]
-        while s:
-            n = s.pop()
-            if n is None:
-                rst.append('')
-                continue
-            rst.append('0')
-            s.insert(0, n.left)
-            s.insert(0, n.right)
-
-        return ','.join(rst)
-
-    def add2NodesToTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode, TreeNode
-        """
-        a = TreeNode(0)
-        a.left = root
-        a.right = TreeNode(0)
-
-        b = TreeNode(0)
-        b.right = root
-        b.left = TreeNode(0)
-
-        return a, b
-
     def allPossibleFBT(self, N):
         """
         :type N: int
@@ -88,24 +45,21 @@ class Solution(object):
 
         for i in range(3, 1+N, 2):
             s = set()
-            serializedTree = set()
             for j in range(1, i, 2):
                 for rootL in dp_arr[j]:
                     for rootR in dp_arr[i-j-1]:
                         new_root = TreeNode(0)
                         new_root.left = rootL
                         new_root.right = rootR
-                        if new_root not in serializedTree:
-                            serializedTree.add(new_root)
-                            s.add(new_root)
+                        s.add(new_root)
             dp_arr[i] = s
         return list(dp_arr[N])
 
 
 def main():
     s = Solution()
-    for root in s.allPossibleFBT(21):
-        print s.serialize(root)
+    for i in range(21):
+        print len(s.allPossibleFBT(i))
 
 
 if __name__ == '__main__':
