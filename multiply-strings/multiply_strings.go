a99045e454b475ac08f8b291fa33a637d3c5c3e9
childe <rmself@qq.com>
2019-09-01 19:41:33 +0800

    Accepted
    Runtime: 0 ms

--- multiply-strings/multiply_strings.go
+++ multiply-strings/multiply_strings.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"strings"
-)
-
 /*
 https://leetcode.com/problems/multiply-strings/
 
@@ -26,119 +22,28 @@ Note:
 
 */
 
-func addString(num1 string, num2 string) string {
-	if len(num1) > len(num2) {
-		return addString(num2, num1)
-	}
-	rst := make([]byte, len(num2)+1)
-	rst[0] = '0'
-
-	var c, c1, c2 byte
-	len1, len2 := len(num1), len(num2)
-	diff := len2 - len1
-
-	copy(rst[1:], num2[0:diff])
-
-	for i := 0; i < len(num1); i++ {
-		c1 = num1[i] - '0'
-		c2 = num2[diff+i] - '0'
-
-		c = (c1 + c2)
-		rst[diff+i+1] = c + '0'
-	}
-
-	for i := len(rst) - 1; i >= 0; i-- {
-		if rst[i] > '9' {
-			rst[i] -= 10
-			rst[i-1] += 1
-		}
-	}
-
-	if rst[0] == '0' {
-		return string(rst[1:])
-	}
-	return string(rst)
-}
-
-type NumZero struct {
-	num       string
-	zeroCount int
-}
-
-func (n1 *NumZero) add(n2 *NumZero) *NumZero {
-	if n1.num == "0" {
-		return n2
-	}
-	if n2.num == "0" {
-		return n1
-	}
-
-	if n1.zeroCount > n2.zeroCount {
-		return n2.add(n1)
-	}
-
-	s := addString(n1.num, n2.num+strings.Repeat("0", n2.zeroCount-n1.zeroCount))
-
-	return &NumZero{s, n1.zeroCount}
-}
-
 func multiply(num1 string, num2 string) (n string) {
-	n1list := GenNZeroList(num1)
-	n2list := GenNZeroList(num2)
-
-	rst := &NumZero{"0", 0}
-	for _, n1 := range n1list {
-		for _, n2 := range n2list {
-			rst = rst.add(&NumZero{itoa(n1.n * n2.n), n1.zeroCount + n2.zeroCount})
-		}
-	}
-
-	return rst.num + strings.Repeat("0", rst.zeroCount)
-}
-
-type NZero struct {
-	n         int
-	zeroCount int
-}
-
-func GenNZeroList(num string) []*NZero {
-	var partLength = 9
-	var n string
-	rst := make([]*NZero, 0)
-	for len(num) > partLength {
-		n = num[:partLength]
-		rst = append(rst, &NZero{atoi(n), len(num) - len(n)})
-		num = num[partLength:]
-	}
-	if len(num) > 0 {
-		rst = append(rst, &NZero{atoi(num), 0})
-	}
-	return rst
-}
-
-func atoi(s string) (n int) {
-	for _, c := range s {
-		n = 10*n + int(c) - '0'
-	}
-	return
-}
-
-func itoa(n int) string {
-	if n == 0 {
+	if num1 == "0" || num2 == "0" {
 		return "0"
 	}
 
-	rst := make([]byte, 0)
-	for n > 0 {
-		rst = append(rst, byte(n%10+'0'))
-		n = n / 10
+	len1, len2 := len(num1), len(num2)
+	r := make([]rune, len1+len2)
+	r[0] = '0'
+
+	for i, c1 := range []rune(num1) {
+		for j, c2 := range []rune(num2) {
+			r[1+i+j] += (c1 - '0') * (c2 - '0')
+		}
 	}
 
-	l := len(rst)
-
-	for i := 0; i < l/2; i++ {
-		rst[i], rst[l-1-i] = rst[l-1-i], rst[i]
+	for i := len(r) - 1; i > 0; i-- {
+		r[i-1] += r[i] / 10
+		r[i] = r[i]%10 + '0'
 	}
 
-	return string(rst)
+	if r[0] == '0' {
+		return string(r[1:])
+	}
+	return string(r)
 }
--- multiply-strings/multiply_strings_test.go
+++ multiply-strings/multiply_strings_test.go
@@ -2,71 +2,6 @@ package main
 
 import "testing"
 
-func TestAddString(t *testing.T) {
-	var num1, num2, output, myrst string
-
-	num1 = "1"
-	num2 = "1"
-	output = "2"
-
-	myrst = addString(num1, num2)
-	if myrst != output {
-		t.Errorf("%q %q %q %q", num1, num2, output, myrst)
-	}
-
-	num1 = "56"
-	num2 = "789"
-	output = "845"
-
-	myrst = addString(num1, num2)
-	if myrst != output {
-		t.Errorf("%s %s %s %s %q %q", num1, num2, output, myrst, output, myrst)
-	}
-
-	num1 = "46"
-	num2 = "58"
-	output = "104"
-
-	myrst = addString(num1, num2)
-	if myrst != output {
-		t.Errorf("%s %s %s %s %q %q", num1, num2, output, myrst, output, myrst)
-	}
-
-	num1 = "9999"
-	num2 = "1"
-	output = "10000"
-
-	myrst = addString(num1, num2)
-	if myrst != output {
-		t.Errorf("%s %s %s %s %q %q", num1, num2, output, myrst, output, myrst)
-	}
-}
-
-func TestAdd(t *testing.T) {
-	var n1, n2, myrst *NumZero
-
-	n1 = &NumZero{"2", 0}
-	n2 = &NumZero{"3", 0}
-	myrst = n1.add(n2)
-	if myrst.num != "5" || myrst.zeroCount != 0 {
-		t.Error("")
-	}
-
-	n1 = &NumZero{"567", 3}
-	n2 = &NumZero{"789", 4}
-	myrst = n1.add(n2)
-	if myrst.num != "8457" || myrst.zeroCount != 3 {
-		t.Error("")
-	}
-
-	n1 = &NumZero{"0", 3}
-	n2 = &NumZero{"789", 4}
-	myrst = n1.add(n2)
-	if myrst.num != "789" || myrst.zeroCount != 4 {
-		t.Error("")
-	}
-}
-
 func TestMultiply(t *testing.T) {
 	var (
 		num1 string
@@ -75,15 +10,6 @@ func TestMultiply(t *testing.T) {
 		output string
 		myrst  string
 	)
-
-	num1 = "10000000000000000000000000000001"
-	num2 = "10000000000000000000000000000001"
-	output = "100000000000000000000000000000020000000000000000000000000000001"
-
-	myrst = multiply(num1, num2)
-	if myrst != output {
-		t.Errorf("%s %s %s %s %q", num1, num2, output, myrst, myrst)
-	}
 
 	num1 = "0"
 	num2 = "0"
@@ -142,6 +68,15 @@ func TestMultiply(t *testing.T) {
 	num1 = "1231231234567888987654566667788"
 	num2 = "98765645786970968595678987654345678876543456789"
 	output = "121603347995187089387245547846447256361514596486207950723079232688750496212732"
+	myrst = multiply(num1, num2)
+	if myrst != output {
+		t.Errorf("%s %s %s %s %q", num1, num2, output, myrst, myrst)
+	}
+
+	num1 = "10000000000000000000000000000001"
+	num2 = "10000000000000000000000000000001"
+	output = "100000000000000000000000000000020000000000000000000000000000001"
+
 	myrst = multiply(num1, num2)
 	if myrst != output {
 		t.Errorf("%s %s %s %s %q", num1, num2, output, myrst, myrst)
