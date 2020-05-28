332ab5a6b5011bc271ff7fa08af9306c1c39a4f4
childe <rmself@qq.com>
2020-05-28 18:59:08 +0800

    AC
    执行结果： 通过 显示详情 执行用时 : 220 ms , 在所有 Python 提交中击败了 60.61% 的用户 内存消耗 : 13.9 MB , 在所有 Python 提交中击败了 100.00% 的用户

--- word-search/solution.py
+++ word-search/solution.py
@@ -29,6 +29,7 @@ board and word consists only of lowercase and uppercase English letters.
 1 <= word.length <= 10^3
 """
 
+
 class Solution(object):
     def exist(self, board, word):
         """
@@ -36,3 +37,67 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
+        new_board = [['']+row+[''] for row in board]
+        new_board.insert(0, ['']*(2+len(board[0])))
+        new_board.append(['']*(2+len(board[0])))
+        board = new_board
+
+        candidates = []
+        for i, row in enumerate(board):
+            for j, c in enumerate(row):
+                if c == word[0]:
+                    candidates.append((i, j))
+
+        print(candidates)
+        for i, j in candidates:
+            print(i, j)
+            if self.p(i, j, board, word):
+                return True
+        return False
+
+    def p(self, i, j, board, word):
+        directions = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+        step, dir_idx, original_char = 1, 0, board[i][j]
+        stack = [[i, j, step, original_char, dir_idx]]
+        board[i][j] = ''
+        while stack:
+            print(len(stack))
+            print(stack[-1])
+            i, j, step, original_char, dir_idx = stack[-1]
+            if step == len(word):
+                return True
+            # 需要回溯
+            if dir_idx == len(directions):
+                board[i][j] = original_char
+                stack.pop()
+                continue
+            stack[-1][-1] += 1
+            x, y = directions[dir_idx]
+            if board[i+x][j+y] == word[step]:
+                board[i+x][j+y] = ''
+                stack.append([i+x, j+y, step+1, word[step], 0])
+
+        return False
+
+
+def main():
+    s = Solution()
+
+    board = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+    ]
+    assert s.exist(board, "ABCCED") is True
+    assert s.exist(board, "SEE") is True
+    assert s.exist(board, "ABCB") is False
+
+    board = [
+        ['A', 'A'],
+    ]
+    assert s.exist(board, "AAA") is False
+
+
+if __name__ == '__main__':
+    main()
