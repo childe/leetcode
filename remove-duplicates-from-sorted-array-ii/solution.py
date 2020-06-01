90ae3295fdc304732b985cc30e9a5eb0b3e36a86
childe <rmself@qq.com>
2020-06-01 23:34:59 +0800

    AC
    执行用时 : 28 ms , 在所有 Python 提交中击败了 54.56% 的用户 内存消耗 : 12.9 MB , 在所有 Python 提交中击败了 100.00% 的用户

--- remove-duplicates-from-sorted-array-ii/solution.py
+++ remove-duplicates-from-sorted-array-ii/solution.py
@@ -2,29 +2,35 @@
 
 """
 https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
-Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared
+at most twice and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input
+array in-place with O(1) extra memory.
 
 Example 1:
 
 Given nums = [1,1,1,2,2,3],
 
-Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+Your function should return length = 5, with the first five elements of nums being
+1, 1, 2, 2 and 3 respectively.
 
 It doesn't matter what you leave beyond the returned length.
 Example 2:
 
 Given nums = [0,0,1,1,1,1,2,3,3],
 
-Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+Your function should return length = 7, with the first seven elements of nums being
+modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
 
 It doesn't matter what values are set beyond the returned length.
 Clarification:
 
 Confused why the returned value is an integer but your answer is an array?
 
-Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Note that the input array is passed in by reference, which means modification to the
+input array will be known to the caller as well.
 
 Internally you can think of this:
 
@@ -45,3 +51,55 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+
+        i, j, l = 0, 0, len(nums)
+        while j < l:
+            # print(i,j,l,nums[:l])
+            if nums[i] == nums[j]:
+                j += 1
+                continue
+            if j - i <= 2:  ## 不需要移动
+                i = j
+                j += 1
+                continue
+            diff = j - i - 2
+            for idx in range(j, len(nums)):
+                nums[idx - diff] = nums[idx]
+            l -= diff
+            i += 2
+            j = i + 1
+
+        ## 最后还要检查一次
+        if j -i <= 2:
+            return l
+        diff = j - i - 2
+        l -= diff
+        return l
+
+
+def main():
+    s = Solution()
+    nums = [1, 1, 1]
+    ans = s.removeDuplicates(nums)
+    print(ans)
+    print(nums)
+    assert ans == 2
+    assert nums[:ans] == [1, 1]
+
+    nums = [1, 1, 1, 2, 2, 3]
+    ans = s.removeDuplicates(nums)
+    print(ans)
+    print(nums)
+    assert ans == 5
+    assert nums[:ans] == [1, 1, 2, 2, 3]
+
+    nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+    ans = s.removeDuplicates(nums)
+    assert ans == 7
+    assert nums[:ans] == [0, 0, 1, 1, 2, 3, 3]
+
+
+if __name__ == "__main__":
+    main()
