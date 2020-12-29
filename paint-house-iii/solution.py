680e7eb690adf5488fde433a77fd6948647fe2a2
childe <heychilde@gmail.com>
2020-12-30 00:17:28 +0800

    timeout

--- paint-house-iii/solution.py
+++ paint-house-iii/solution.py
@@ -52,7 +52,33 @@ cost[i][j]：是将第 i 个房子涂成颜色 j+1 的花费。
 
 
 class Solution(object):
-    def minCost(self, houses, cost, m, n, target):
+    def __init__(self):
+        self.expenditures = []
+
+    def blockCount(self, houses):
+        """
+        >>> s = Solution()
+        >>> s.blockCount([3,1,2,3])
+        4
+        >>> s.blockCount([1,1,1,1])
+        1
+        >>> s.blockCount([1,1,2,1])
+        3
+        """
+        rst = 0
+        pre = None
+        for n in houses:
+            if n == pre:
+                continue
+            rst += 1
+            pre = n
+
+        return rst
+
+    def noZero(self, houses):
+        return all([e != 0 for e in houses])
+
+    def reMinCost(self, houses, cost, m, n, target, zeroCount, expenditure):
         """
         :type houses: List[int]
         :type cost: List[List[int]]
@@ -61,10 +87,56 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        if zeroCount == 0:
+            if self.blockCount(houses) != target:
+                return -1
+            return expenditure
+
+        for i, h in enumerate(houses):
+            if h == 0:
+                for j, c in enumerate(cost[i]):
+                    r = self.reMinCost(houses[:i]+[j+1] + houses[i+1:], cost, m, n, target, zeroCount-1, expenditure+c)
+                    if r != -1:
+                        self.expenditures.append(r)
+
+        if self.expenditures:
+            return min(self.expenditures)
+        return -1
+
+    def minCost(self, houses, cost, m, n, target):
+        zeroCount = sum([h == 0 for h in houses])
+        return self.reMinCost(houses, cost, m, n, target, zeroCount, 0)
 
 
 def main():
     s = Solution()
+
+    houses = [0, 2, 1, 2, 0]
+    cost = [[1, 10], [10, 1], [10, 1], [1, 10], [5, 1]]
+    m = 5
+    n = 2
+    target = 3
+    ans = s.minCost(houses, cost, m, n, target)
+    print(ans)
+    assert ans == 11
+
+    houses = [0, 0, 0, 0, 0]
+    cost = [[1, 10], [10, 1], [10, 1], [1, 10], [5, 1]]
+    m = 5
+    n = 2
+    target = 3
+    ans = s.minCost(houses, cost, m, n, target)
+    print(ans)
+    assert ans == 9
+
+    houses = [3, 1, 2, 3]
+    cost = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    m = 4
+    n = 3
+    target = 3
+    ans = s.minCost(houses, cost, m, n, target)
+    print(ans)
+    assert ans == -1
 
 
 if __name__ == "__main__":
