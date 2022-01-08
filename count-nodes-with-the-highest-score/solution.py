62d39607af1ab62bb2b836ff3d789cf4a0092409
childe <rmself@qq.com>
2022-01-09 01:03:16 +0800

    RecursionError: maximum recursion depth exceeded

--- count-nodes-with-the-highest-score/solution.py
+++ count-nodes-with-the-highest-score/solution.py
@@ -17,55 +17,54 @@ class Solution:
         >>> s.countHighestScoreNodes(parents = [-1,2,0])
         2
         """
-        children = []
-        for _ in parents:
-            children.append([])
+        n = len(parents)
 
+        children = [[] for _ in parents]
         for i, p in enumerate(parents):
             if p != -1:
                 children[p].append(i)
+
         # print(f"{children=}")
-        # print(len(children))
 
-        # init childrenCount to [{left:Count, right:Count} ...]
-        childrenCount = [{}] * len(parents)
-        for i, _ in enumerate(children):
-            childrenCount[i] = dict()
+        max_count, ans = 0, 0
+        step = 0
 
-        ## filll childrenCount from down to up
-        for i, c in enumerate(children):
-            # print(f"{i=}")
-            if c == []:
-                p, childeNode = parents[i], i
-                while p != -1:
-                    childrenCount[p][childeNode] = 1 + sum(
-                        childrenCount[childeNode].values()
-                    )
-                    if len(children[p]) != len(childrenCount[p]):
-                        break
-                    p, childeNode = parents[p], p
+        def postOrderVisit(node):
+            nonlocal step
+            step += 1
+            nonlocal max_count, ans
 
-        # print(f"{childrenCount=}")
+            count, r = 1, 1
+            for c in children[node]:
+                v = postOrderVisit(c)
+                count *= v
+                r += v
+            count *= (n - r) or 1
+            if count == max_count:
+                ans += 1
+            elif count > max_count:
+                max_count, ans = count, 1
+            return r
 
-        totalCount = len(parents)
-        removed = []  # 删除一个节点之后，分出来的所有树的 Node 数量
-        for i, c in enumerate(childrenCount):
-            removed.append(list(c.values()))
-            remainingCount = totalCount - sum(c.values()) - 1
-            if remainingCount > 0:
-                removed[i].append(remainingCount)
+        postOrderVisit(0)
 
-        # print(f"{removed=}")
-
-        r = [reduce(lambda x, y: x * y, e, 1) for e in removed]
-        return r.count(max(r))
+        print(f"{step=}")
+        return ans
 
 
 def main():
-    parents = eval(open("./testcase").read())
-    print(len(parents))
     s = Solution()
-    s.countHighestScoreNodes(parents)
+    ans = s.countHighestScoreNodes(parents=[-1, 2, 0, 2, 0])
+    print(ans)
+    ans = s.countHighestScoreNodes(parents=[-1, 2, 0])
+    print(ans)
+
+    parents = eval(open("./testcase").read())
+    s = Solution()
+    ans = s.countHighestScoreNodes(parents)
+    print(ans)
+
+    return
 
 
 if __name__ == "__main__":
