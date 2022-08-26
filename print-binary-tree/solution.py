6949021778cd91665e3aaaa234e0a43328d6899a
childe <rmself@qq.com>
2022-08-26 15:57:48 +0800

    AC

--- print-binary-tree/solution.py
+++ print-binary-tree/solution.py
@@ -6,7 +6,7 @@ https://leetcode.cn/problems/print-binary-tree/
 Given the root of a binary tree, construct a 0-indexed m x n string matrix res that represents a formatted layout of the tree. The formatted layout matrix should be constructed using the following rules:
 
 The height of the tree is height and the number of rows m should be equal to height + 1.
-The number of columns n should be equal to 2height+1 - 1.
+The number of columns n should be equal to 2^(height+1) - 1.
 Place the root node in the middle of the top row (more formally, at location res[0][(n-1)/2]).
 For each node that has been placed in the matrix at position res[r][c], place its left child at res[r+1][c-2height-r-1] and its right child at res[r+1][c+2height-r-1].
 Continue this process until all the nodes in the tree have been placed.
@@ -45,11 +45,54 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
-    def printTree(self, root: TreeNode|None) -> list[list[str]]:
+    def height(self, root: TreeNode) -> int:
+        """
+        >>> s = Solution()
+        >>> root = TreeNode(1)
+        >>> root.left = TreeNode(2)
+        >>> s.height(root)
+        1
+        >>> s.height(root.left)
+        0
+        """
+        left_height, right_height = 0, 0
+        if root.left is None and root.right is None:
+            return 0
+
+        if root.left is not None:
+            left_height = self.height(root.left)
+        if root.right is not None:
+            right_height = self.height(root.right)
+
+        return max(left_height, right_height) + 1
+
+    def printTree(self, root: TreeNode | None) -> list[list[str]]:
+        """
+        >>> s = Solution()
+        >>> root = TreeNode(1)
+        >>> root.left = TreeNode(2)
+        >>> s.printTree(root)
+        [['', 1, ''], [2, '', '']]
+        """
         if root is None:
             return []
 
+        height = self.height(root)
         rst = []
+        col_count = 2 ** (height + 1) - 1
+        for i in range(height+1):
+            rst.append([""] * col_count)
 
+        def putNode(node: TreeNode, height: int, left: int, right: int):
+            p = (left + right) // 2
+            rst[height][p] = str(node.val)
+            if node.left:
+                putNode(node.left, height + 1, left, p)
+            if node.right:
+                putNode(node.right, height + 1, p, right)
+
+        putNode(root, height=0, left=0, right=col_count)
         return rst
