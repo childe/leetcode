9cf9505cfb311a8ba7109fb98dd373cf81c150be
childe <rmself@qq.com>
2024-01-27 15:01:33 +0800

    AC

--- rotate-image/solution.py
+++ rotate-image/solution.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 https://leetcode-cn.com/problems/rotate-image/
 
 You are given an n x n 2D matrix representing an image.
@@ -44,7 +44,7 @@ rotate the input matrix in-place such that it becomes:
   [16, 7,10,11]
 ]
 
-'''
+"""
 
 
 def p(matrix):
@@ -59,65 +59,43 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        for level in range(0, (1+n)//2):
-            for i in range(level, n-1-level):
-                print(level, i)
-                x, y = level, i
-                num = matrix[x][y]
+        for i in range((n + 1) // 2):
+            start, end = i, n - 1 - i
+            for j in range(start, end):
+                tmp = matrix[i][j]
+                y, x = i, j
                 for _ in range(4):
-                    nx, ny = y, n-x-1
-                    num, matrix[nx][ny] = matrix[nx][ny], num
+                    ny, nx = x, n - 1 - y
+                    matrix[ny][nx], tmp = tmp, matrix[ny][nx]
                     x, y = nx, ny
-                p(matrix)
 
 
 def main():
     s = Solution()
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-        ]
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    a = [
-        [7, 4, 1],
-        [8, 5, 2],
-        [9, 6, 3]
-        ]
+    a = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
 
     s.rotate(matrix)
     p(matrix)
     assert a == matrix
 
-    matrix = [
-        [5, 1, 9, 11],
-        [2, 4, 8, 10],
-        [13, 3, 6, 7],
-        [15, 14, 12, 16]
-        ]
+    matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
 
-    a = [
-        [15, 13, 2, 5],
-        [14, 3, 4, 1],
-        [12, 6, 8, 9],
-        [16, 7, 10, 11]
-        ]
+    a = [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
     s.rotate(matrix)
     p(matrix)
     assert a == matrix
 
-    matrix = [
-        [1, 2],
-        [3, 4]
-    ]
-    a = [
-        [3, 1],
-        [4, 2]
-    ]
+    matrix = [[1, 2], [3, 4]]
+    a = [[3, 1], [4, 2]]
     s.rotate(matrix)
     p(matrix)
     assert a == matrix
 
+    s.rotate([])
+    s.rotate([1])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
