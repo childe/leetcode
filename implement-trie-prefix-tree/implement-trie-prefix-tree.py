1e94179726e91bd42c65ac9395559560115f481d
childe <rmself@qq.com>
2024-10-23 09:34:39 +0800

    new file:   implement-trie-prefix-tree.py

---
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+https://leetcode.cn/problems/implement-trie-prefix-tree/submissions/
+"""
+
+
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+    def insert(self, word: str) -> None:
+        node = self
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Trie()
+            node = node.children[c]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
